6570558a676e6fd60a3279dcb0a56739816a2ceb
mark <machf@coscon.com>
2020-02-07 22:20:59 +0800

    win 5 commit  request - requests

--- maapp/views.py
+++ maapp/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    import request
+    import requests
     import json
-    api_request = request.get("https://api.github.com/users?since=0")
+    api_request = requests.get("https://api.github.com/users?since=0")
     api = json.loads(api_request.content)
     return render(request,'home.html',{"api":api})
 
