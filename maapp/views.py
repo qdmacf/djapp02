dc1be3e81f37c316c2afba14574e4e990d299a57
malinux <malinux@aliyun.cn>
2020-02-07 22:24:35 +0800

    from linux local update one thing.

--- maapp/views.py
+++ maapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    import request
+    import requests
     import json
     api_request = request.get("https://api.github.com/users?since=0")
     api = json.loads(api_request.content)
