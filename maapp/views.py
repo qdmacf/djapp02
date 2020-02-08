75fec4fe3d024700d5c72c093c9b5c16d1f1d5e0
mark <machf@coscon.com>
2020-02-08 11:45:24 +0800

    win commit user urls\views and templates/user.html

--- maapp/views.py
+++ maapp/views.py
@@ -8,4 +8,31 @@ def home(request):
     api = json.loads(api_request.content)
     return render(request,'home.html',{"api":api})
 
+def user(request):
+    if request.method == 'POST':
+        import requests
+        import json
+
+        user = request.POST['user']
+        user_request = requests.get("https://api.github.com/users/"+user)
+        username = json.loads(user_request.content)
+
+        return render(request, 'user.html', {'username':username})
+    else:
+        notfound ="请在搜索框里输入你要查询的用户。。。"
+        return render(request, 'user.html', {'notfound':notfound})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
